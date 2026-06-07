5776b532ccc19072d74085948b78034c03bfa271
b4kgaming1223-collab <b4kgaming1223@gmail.com>
2026-06-07 15:51:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
-    // 📱 ඔයාගේ WhatsApp නම්බර් එක (උදා: "94740237746" - රටේ කෝඩ් එකත් එක්ක දාන්න)
+    // 📱 ඔයාගේ WhatsApp නම්බර් එක (උදා: "947XXXXXXXX" - රටේ කෝඩ් එකත් එක්ක දාන්න)
     MY_NUMBER: "94740237746",
 
-    // 👑 බොට්ගේ අයිතිකාරයාගේ විස්තර
+    // 👑 බොට්ගේ අයිතිකාරයාගේ විස්තර සහ Realm එක
     OWNER_NAME: "Liyo",
     OWNER_REALM: "Soul Land Realm",
     
-    // 🖼️ Xiao Wu ගේ ලස්සනම Photo Direct Link එක
+    // 🖼️ Xiao Wu ගේ ලස්සනම Photo Direct Link එක (Menu/Alive වලට යන එක)
     BOT_IMAGE: "https://raw.githubusercontent.com/b4kgaming1223-collab/XIAO-WU-MD-help/refs/heads/main/1780583805266.png",
 
-    // 🎵 Menu එක ගහද්දි ඔටෝම යන Voice Note (Direct MP3 Link)
+    // 🎵 Menu එක ගැහුවාම ඔටෝම බැක්ග්‍රවුන්ඩ් එකෙන් යන Voice Note (Direct MP3 Link)
     MENU_AUDIO: "https://github.com/Sadeesha-lara/Lara-MD-Bot/raw/main/media/menu.mp3",
 
-    // 🎵 Alive එක ගහද්දි ඔටෝම යන Voice Note (Direct MP3 Link)
+    // 🎵 Alive එක ගැහුවාම ඔටෝම බැක්ග්‍රවුන්ඩ් එකෙන් යන Voice Note (Direct MP3 Link)
     ALIVE_AUDIO: "https://github.com/Sadeesha-lara/Lara-MD-Bot/raw/main/media/alive.mp3"
 };
